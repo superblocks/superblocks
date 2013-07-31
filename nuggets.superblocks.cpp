240659d157f66ce0f0301cf8732dbf30245b3e12
superblocks <superblocks@github.com>
2013-07-31 13:13:14 +0000

    v0.4 bettah crazy

--- nuggets.superblocks.cpp
+++ nuggets.superblocks.cpp
@@ -1,18 +1,59 @@
 //
-// Predict next block reward for:
-// Nuggets (NUG) - normal reward: 49 - superblock: 10045 - repo: https://bitbucket.org/mytwobits/nuggets/
+// Predict next block reward for: Nuggets (NUG)
+// reward -  normal: 49 - superblock: 10045 - repo: https://bitbucket.org/mytwobits/nuggets/
 //
-// Version 0.3
+// Version 0.4
 //
 // superblock test:  ./nug 86b707fad8477ce7c30bcfdf90e4371bc45751bf76f4b3c3253e3d16a7cc4be3 debug
 //
 #include <stdio.h>
 #include <string.h>
-#include <string>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 using namespace std;
 using namespace boost;
+
+int static generateMTRandom(unsigned int s, int range) {
+        random::mt19937 gen(s);
+        random::uniform_int_distribution<> dist(1, range);
+        return dist(gen);
+}
+
+static const long hextable[] = {
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 10-19
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 30-39
+        -1, -1, -1, -1, -1, -1, -1, -1,  0,  1,
+         2,  3,  4,  5,  6,  7,  8,  9, -1, -1,         // 50-59
+        -1, -1, -1, -1, -1, 10, 11, 12, 13, 14,
+        15, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 70-79
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, 10, 11, 12,         // 90-99
+        13, 14, 15, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 110-109
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 130-139
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 150-159
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 170-179
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 190-199
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 210-219
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // 230-239
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1 };
+
+long hex2long(const char* hexString) {
+        long ret = 0;
+        while (*hexString && ret >= 0) {
+                ret = (ret << 4) | hextable[*hexString++];
+        }
+        return ret;
+}
 
 int main( int argc, char **argv ) {
 
@@ -24,8 +65,7 @@ int main( int argc, char **argv ) {
         int substr_start = 55;
         int substr_len = 7;
 
-        int rnd_low = 1;
-        int rnd_high = 1000000;
+        int rnd_limit = 1000000;
         int rnd_lucky = 9723;
 
         int reward_normal = 49;
@@ -51,45 +91,10 @@ int main( int argc, char **argv ) {
         const char* cseed = cseed_str.c_str();
         if( debug ) { printf("cseed: %s\n", cseed); }
 
-        static const long hextable[] = {
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 10-19
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 30-39
-        -1, -1, -1, -1, -1, -1, -1, -1, 0, 1,
-        2, 3, 4, 5, 6, 7, 8, 9, -1, -1, // 50-59
-        -1, -1, -1, -1, -1, 10, 11, 12, 13, 14,
-        15, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 70-79
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, // 90-99
-        13, 14, 15, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 110-109
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 130-139
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 150-159
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 170-179
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 190-199
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 210-219
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 230-239
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1
-        };
-
-        long seed = 0;
-        while (*cseed && seed >= 0) {
-                seed = (seed << 4) | hextable[*cseed++];
-        }
-
+        long seed = hex2long(cseed);
         if( debug ) { printf("seed: %d\n", seed); }
 
-        random::mt19937 gen(seed);
-        random::uniform_int_distribution<> dist(rnd_low, rnd_high);
-        int rand = dist(gen);
+        int rand = generateMTRandom(seed, rnd_limit);
         if( debug ) { printf("rand: %d\n", rand); }
 
         int nSubsidy;
@@ -104,4 +109,3 @@ int main( int argc, char **argv ) {
 
         return 0;
 }
-

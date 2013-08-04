20ee47e3c86c4b60f07893293c489c6425fdbd86
superblocks <superblocks@github.com>
2013-08-04 20:10:00 +0000

    Update superblocks.cpp

--- superblocks.cpp
+++ superblocks.cpp
@@ -8,94 +8,92 @@ int Superblocks::getnextblockreward( const char* hash )
 
 	int hlen = strlen(hash);
 	if( debug ) { 
-			cout << "-------------------" << endl; 
-			cout << "getnextblockreward" << endl; 
-			cout << "srlen(hash): " << hlen << endl; 
+		cout << "-------------------" << endl; 
+		cout << "getnextblockreward" << endl; 
+		cout << "srlen(hash): " << hlen << endl; 
 	}
 
-		if( hlen != hash_length ) { 
-			cout << "error: hash length not " << hash_length << endl;
-			return 0;
-		}
-		if( debug ) { cout << "hash: " << hash << endl; }
+	if( hlen != hash_length ) { 
+		cout << "error: hash length not " << hash_length << endl;
+		return 0;
+	}
+	if( debug ) { cout << "hash: " << hash << endl; }
 
-		string hash_string(hash);
-		if( debug ) { cout << "hash_string: " << hash_string << endl; }
+	string hash_string(hash);
+	if( debug ) { cout << "hash_string: " << hash_string << endl; }
 
-		if( debug ) { cout << "LUCK_STEPS: " << LUCK_STEPS << endl; }
+	if( debug ) { cout << "LUCK_STEPS: " << LUCK_STEPS << endl; }
 
-		int step = 1;
-		if( debug ) { cout << "step: " << step << endl; }
+	int step = 1;
+	if( debug ) { cout << "step: " << step << endl; }
 
-		if( debug ) { cout << "cutout_start: " << cutout_start[step] << endl; }
-		if( debug ) { cout << "cutout_length: " << cutout_length[step] << endl; }
+	if( debug ) { cout << "cutout_start: " << cutout_start[step] << endl; }
+	if( debug ) { cout << "cutout_length: " << cutout_length[step] << endl; }
 
-		std::string cseed_str = hash_string.substr( cutout_start[step], cutout_length[step]);
-		const char* cseed = cseed_str.c_str();
-		if( debug ) { cout << "cseed: " << cseed << endl; }
+	std::string cseed_str = hash_string.substr( cutout_start[step], cutout_length[step]);
+	const char* cseed = cseed_str.c_str();
+	if( debug ) { cout << "cseed: " << cseed << endl; }
 
-		long seed = hex2long(cseed);
-		if( debug ) { cout << "seed: " << seed << endl; }
+	long seed = hex2long(cseed);
+	if( debug ) { cout << "seed: " << seed << endl; }
 
-		if( debug ) { cout << "rnd_range_low: " << rnd_range_low[step] << endl; }
-		if( debug ) { cout << "rnd_range_high: " << rnd_range_high[step] << endl; }
-		int rand = generateMTRandom(seed, rnd_range_high[step]);
-		if( debug ) { cout << "rand: " << rand << endl; }
+	if( debug ) { cout << "rnd_range_low: " << rnd_range_low[step] << endl; }
+	if( debug ) { cout << "rnd_range_high: " << rnd_range_high[step] << endl; }
+	int rand = generateMTRandom(seed, rnd_range_high[step]);
+	if( debug ) { cout << "rand: " << rand << endl; }
 
-		if( debug ) { cout << "luck_type: " << luck_type[step] << endl; }
+	if( debug ) { cout << "luck_type: " << luck_type[step] << endl; }
 
-		int nextreward = reward[0]; // normal reward
-		if( debug ) { cout << "normal reward: " << nextreward << endl; }
+	int nextreward = reward[0]; // normal reward
+	if( debug ) { cout << "normal reward: " << nextreward << endl; }
 
-		for (int rstep = 1; rstep < LUCK_STEPS ; rstep++) {
+	for (int rstep = 1; rstep < LUCK_STEPS ; rstep++) {
 
-			if( debug )
-				cout << "rstep: " << rstep 
-					<< " reward: " << reward[rstep] 
-					<< " rand: " << rand << endl; 
+		if( debug )
+			cout << "rstep: " << rstep << " reward: " << reward[rstep] << " rand: " << rand << endl; 
 
-			switch( luck_type[rstep] ) { 
+		switch( luck_type[rstep] ) { 
 
-				default: 
-					cout << "error: unknown luck type:" << luck_type[rstep] << endl; 
-					return 0; 
+		default: 
+			cout << "error: unknown luck type:" << luck_type[rstep] << endl; 
+			return 0; 
 
-				case 1: // LUCK_WITH_LESS_THAN_X 
+		case 1: // LUCK_WITH_LESS_THAN_X 
 
-					if( debug ) { cout << "luck_x: " << luck_x[rstep] << endl; }
-					if( rand < luck_x[rstep] ) {
-						nextreward = reward[rstep];
-						if( secondary_rand[rstep] == 1 ) { 
-							nextreward = getsecondaryreward( rand, rstep );
-						}
-						goto final_reward;
-					}
-					break;
-
-				case 2: // LUCK_WITH_MORE_THAN_X 
-
-					cout << "TODO luck type 2 MORE THAN X" << endl; 
-					if( debug ) { cout << "luck_x: " << luck_x[rstep] << endl; }
-					return 0;
-
-				case 3: // LUCK_WITH_MORE_THAN_X_AND_LESS_THAN_Y
-
-					if( debug ) { cout << "luck_x: " << luck_x[rstep] << endl; }
-					if( debug ) { cout << "luck_y: " << luck_y[rstep] << endl; }
-					if( rand > luck_x[rstep] && rand < luck_y[rstep] ) {
-						nextreward = reward[rstep];
-					if( secondary_rand[rstep] == 1 ) {
-							nextreward = getsecondaryreward( rand, rstep );
-						}
-						goto final_reward;
-					}
-					break;
+			if( debug ) { cout << "luck_x: " << luck_x[rstep] << endl; }
+			if( rand < luck_x[rstep] ) {
+				nextreward = reward[rstep];
+				if( secondary_rand[rstep] == 1 ) { 
+					nextreward = getsecondaryreward( rand, rstep );
+				}
+				goto final_reward;
 			}
-		}
+			break;
 
-		final_reward:
-		return nextreward;
+		case 2: // LUCK_WITH_MORE_THAN_X 
+
+			cout << "TODO luck type 2 MORE THAN X" << endl; 
+			if( debug ) { cout << "luck_x: " << luck_x[rstep] << endl; }
+			return 0;
+
+		case 3: // LUCK_WITH_MORE_THAN_X_AND_LESS_THAN_Y
+
+			if( debug ) { cout << "luck_x: " << luck_x[rstep] << endl; }
+			if( debug ) { cout << "luck_y: " << luck_y[rstep] << endl; }
+			if( rand > luck_x[rstep] && rand < luck_y[rstep] ) {
+				nextreward = reward[rstep];
+				if( secondary_rand[rstep] == 1 ) {
+					nextreward = getsecondaryreward( rand, rstep );
+				}
+				goto final_reward;
+			}
+			break;
+		}
 	}
+
+	final_reward:
+	return nextreward;
+}
 
 int Superblocks::getsecondaryreward( int seed, int step ) 
 {

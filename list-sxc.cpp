af6995e5599c67d1bb88b8998e51c2b3549cb523
superblocks <superblocks@github.com>
2013-08-04 20:04:00 +0000

    Update list-sxc.cpp

--- list-sxc.cpp
+++ list-sxc.cpp
@@ -1,8 +1,8 @@
 // SXC superblocks list
-// Superblocks - Version 0.4
+// Superblocks - Version 0.4.2
 //
-// verify: explorer: http://sexcoin.lavajumper.com:9950/
-//
+// explorer: http://sexcoin.lavajumper.com:9950/
+
 #include <iostream>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
@@ -12,17 +12,16 @@ using namespace boost;
 int list_length = 100000;
 
 int main() {
-  cout << "[SXC] Sexcoin - Superblock list" << endl << endl;
+	cout << "[SXC] Sexcoin - Superblock list" << endl << endl;
 	cout << "BLOCK, REWARD" << endl;
-        for (int nHeight = 0; nHeight <= list_length; nHeight++) {
+	for (int nHeight = 0; nHeight <= list_length; nHeight++) {
 		random::mt19937 gen(nHeight);
 		random::uniform_int_distribution<> dist(1, 100000);
 		int rand = dist(gen);
 		if(rand > 99990)
-                        cout << nHeight << ", 5000" << endl;
+			cout << nHeight << ", 5000" << endl;
 		else if (rand < 2001) 
-                        cout << nHeight << ", 500" << endl;
-
+			cout << nHeight << ", 500" << endl;
 	}
 	return 0;
 }

edb59e8f383188340031646c57012c3e7e196466
superblocks <superblocks@github.com>
2013-08-04 09:11:15 +0000

    Create next-nug.cpp

---
+++ next-nug.cpp
@@ -0,0 +1,27 @@
+// Get Next Block Reward: [NUG] Nuggets
+// Superblocks - Version 0.4
+
+#include "superblocks.hpp" 
+
+void Superblocks::init() {
+
+  coin = "NUG"; 
+	coin_name = "Nuggets";
+
+	hash_length = 64;
+
+	// normal (step 0)
+	reward[0] = 49;
+
+	// superblock (step 1)
+	reward[1] = 10045; // superblock reward (step 1)
+	seed_type[1] = SEED_WITH_PREVIOUS_BLOCK_HASH;
+	cutout_start[1] = 55;
+	cutout_length[1] = 7;
+	rnd_range_low[1] = 1;
+	rnd_range_high[1] = 1000000;
+	luck_type[1] = LUCK_WITH_LESS_THAN_X;
+	luck_x[1] = 9723;
+
+}
+

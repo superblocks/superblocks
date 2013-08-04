7b55480454e50c44899417d21720de10c796507a
superblocks <superblocks@github.com>
2013-08-04 19:56:12 +0000

    Update precom-nug.cpp

--- precom-nug.cpp
+++ precom-nug.cpp
@@ -1,6 +1,6 @@
 // Precompute superblock hash cutouts: [NUG] Nuggets
-// Superblocks - Version 0.4
-//
+// Superblocks - Version 0.4.2
+
 #include <stdio.h>
 #include <string.h>
 #include <boost/random/mersenne_twister.hpp>
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace boost;
 
-int seed_low  = 0;         // = 0000000
+int seed_low  = 0; // = 0000000
 int seed_high = 268435455; // = fffffff
 
 std::string coin("NUG cutout 55,7");
@@ -21,24 +21,26 @@ int lucky_type = 1; // rand < lucky
 
 //////////////////////////
 
-int static generateMTRandom(unsigned int s, int range) {
-        random::mt19937 gen(s);
-        random::uniform_int_distribution<> dist(1, range);
-        return dist(gen);
+int static generateMTRandom(unsigned int s, int range)
+{
+	random::mt19937 gen(s);
+	random::uniform_int_distribution<> dist(1, range);
+	return dist(gen);
 }
 
-int main( int argc, char **argv ) {
+int main( int argc, char **argv )
+{
 
-  printf("START: cutout search: %s\n", coin.c_str());
+	printf("START: cutout search: %s\n", coin.c_str());
 	printf("START: cutout search: seed_low=%d [%07x], seed_high=%d [%07x]\n", seed_low, seed_low, seed_high, seed_high);
 	printf("START: cutout search: lucky_type=%d, rnd_limit=%d, rnd_lucky=%d, rnd_lucky_low=%d, rnd_lucky_high=%d\n", 
 		lucky_type, rnd_limit, rnd_lucky, rnd_lucky_low, rnd_lucky_high );
 	printf("\nCUTOUT, seed, rand\n");
 
 	int found = 0;
-        long x;
-        for (x = seed_low; x <= seed_high; x++) {
-        	int rand = generateMTRandom(x, rnd_limit);
+	long x;
+	for (x = seed_low; x <= seed_high; x++) {
+		int rand = generateMTRandom(x, rnd_limit);
 		switch( lucky_type ) {
 			default: 
 				printf("error: unknown lucky type\n"); 
@@ -51,9 +53,9 @@ int main( int argc, char **argv ) {
 				break;
 		}
 
-        }
+	}
 	printf("END. found=%d\n", found);
-        return 0;
-		
+	return 0;
+
 }
 

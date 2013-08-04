269607e36e70edea5103acc9718f360e01a27256
superblocks <superblocks@github.com>
2013-08-04 09:09:20 +0000

    Create list-jkc.cpp

---
+++ list-jkc.cpp
@@ -0,0 +1,28 @@
+// JKC superblocks list
+// Superblocks - Version 0.4
+
+#include <iostream>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+using namespace std;
+using namespace boost;
+
+int list_length = 100000;
+
+int main() {
+
+  cout << "[JKC] Junkcoin - Superblock list" << endl << endl;
+	cout << "BLOCK, REWARD" << endl;
+        for (int nHeight = 0; nHeight <= list_length; nHeight++) {
+                random::mt19937 gen(nHeight);
+                random::uniform_int_distribution<> dist(1, 100000);
+                int rand = dist(gen);
+                if(rand > 99990) {
+                        cout << nHeight << ", 1000" << endl;
+                } else if (rand < 1001) {
+                        cout << nHeight << ", 150" << endl;
+                } 
+        }
+        return 0;
+}
+

13f701e72f8602a4073189be60ad1aa4d4be84e0
superblocks <superblocks@github.com>
2013-08-04 20:04:37 +0000

    Update list-jkc.cpp

--- list-jkc.cpp
+++ list-jkc.cpp
@@ -1,5 +1,8 @@
 // JKC superblocks list
-// Superblocks - Version 0.4
+// Superblocks - Version 0.4.2
+//
+// explorer: http://block.al.tcoin.info/chain/JKCCoin
+// explorer: http://jkc.cryptocoinexplorer.com/
 
 #include <iostream>
 #include <boost/random/mersenne_twister.hpp>
@@ -11,18 +14,18 @@ int list_length = 100000;
 
 int main() {
 
-  cout << "[JKC] Junkcoin - Superblock list" << endl << endl;
+	cout << "[JKC] Junkcoin - Superblock list" << endl << endl;
 	cout << "BLOCK, REWARD" << endl;
-        for (int nHeight = 0; nHeight <= list_length; nHeight++) {
-                random::mt19937 gen(nHeight);
-                random::uniform_int_distribution<> dist(1, 100000);
-                int rand = dist(gen);
-                if(rand > 99990) {
-                        cout << nHeight << ", 1000" << endl;
-                } else if (rand < 1001) {
-                        cout << nHeight << ", 150" << endl;
-                } 
-        }
-        return 0;
+	for (int nHeight = 0; nHeight <= list_length; nHeight++) {
+		random::mt19937 gen(nHeight);
+		random::uniform_int_distribution<> dist(1, 100000);
+		int rand = dist(gen);
+		if(rand > 99990) {
+			cout << nHeight << ", 1000" << endl;
+		} else if (rand < 1001) {
+			cout << nHeight << ", 150" << endl;
+		} 
+	}
+	return 0;
 }
 
